a86c8e6949edf72af77344a9f8633a139523b0ef
m0602775-cmyk <m0602775@gmail.com>
2025-12-15 12:50:26 +0300

    not found page done

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+      <div className="w-full max-w-lg rounded-3xl bg-white p-10 text-center ">
+        
+        <h1 className="text-[40px] font-extrabold text-gray-900">404</h1>
+        
+        <p className="mt-4 text-[29px] font-semibold text-gray-800">
+          Sahifa topilmadi
+        </p>
+
+        <p className="mt-3 text-[15px] text-gray-500 leading-10">
+          Siz kiritgan manzil mavjud emas yoki sahifa o'chirilgan.
+          Iltimos, bosh sahifaga qayting.
+        </p>
+
+        <Link
+          to="/"
+          className="mt-8 block w-full rounded-2xl bg-blue-600 py-3
+                     text-base  text-white hover:bg-blue-700"
+        >
+          Homega qaytish
+        </Link>
+
+      </div>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
