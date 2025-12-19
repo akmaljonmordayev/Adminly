c11f39a637f27b0381d2b4149ae1d7dc6d624e12
xojarxon4 <xojarxon4@gmail.com>
2025-12-19 14:23:05 +0500

    sidebar

--- src/role/Employee/NotFound.jsx
+++ src/role/Employee/NotFound.jsx
@@ -5,16 +5,15 @@ function NotFound() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
       <div className="w-full max-w-lg rounded-3xl bg-white p-10 text-center ">
-        
         <h1 className="text-[40px] font-extrabold text-gray-900">404</h1>
-        
+
         <p className="mt-4 text-[29px] font-semibold text-gray-800">
           Sahifa topilmadi
         </p>
 
         <p className="mt-3 text-[15px] text-gray-500 leading-10">
-          Siz kiritgan manzil mavjud emas yoki sahifa o'chirilgan.
-          Iltimos, bosh sahifaga qayting.
+          Siz kiritgan manzil mavjud emas yoki sahifa o'chirilgan. Iltimos, bosh
+          sahifaga qayting.
         </p>
 
         <Link
@@ -24,7 +23,6 @@ function NotFound() {
         >
           Homega qaytish
         </Link>
-
       </div>
     </div>
   );
