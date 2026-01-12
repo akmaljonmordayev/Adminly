d0f39cb72b5043ae8c45ee71c233c6c0e78b9257
Abdullohabdumalikov <a3766668@gmail.com>
2026-01-12 10:55:01 +0500

    archieve page done

--- src/role/Manager/Archieve.jsx
+++ src/role/Manager/Archieve.jsx
@@ -1,54 +1,38 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 function Archieve() {
   return (
     <>
       <nav class="bg-[#0b1220] text-white w-100% min-h-50px p-5 rounded-xl mt-2">
         <ul class="flex gap-6">
-          <Link to={"/manager/archieve/employeesArchieve"}>
+          <Link to={'/manager/archieve/employeesArchieve'}>
             <li class="px-4 py-2 text-gray-300 hover:bg-cyan-400/10 hover:text-cyan-300 rounded-xl cursor-pointer">
               Employees
             </li>
           </Link>
-          <Link to={"/manager/archieve/tasksArchieve"}>
+          <Link to={'/manager/archieve/tasksArchieve'}>
             <li class="px-4 py-2 text-gray-300 hover:bg-cyan-400/10 hover:text-cyan-300 rounded-xl cursor-pointer">
               Tasks
             </li>
           </Link>
-          <Link to={"/manager/archieve/complaintsArchieve"}>
+          <Link to={'/manager/archieve/complaintsArchieve'}>
             <li class="px-4 py-2 text-gray-300 hover:bg-cyan-400/10 hover:text-cyan-300 rounded-xl cursor-pointer">
               Complaints
             </li>
           </Link>
-          <Link to={"/manager/archieve/announcementsArchieve"}>
+          <Link to={'/manager/archieve/announcementsArchieve'}>
             <li class="px-4 py-2 text-gray-300 hover:bg-cyan-400/10 hover:text-cyan-300 rounded-xl cursor-pointer">
               Announcements
             </li>
           </Link>
-          <Link to={"/manager/archieve/financeArchieve"}>
-            <li class="px-4 py-2 text-gray-300 hover:bg-cyan-400/10 hover:text-cyan-300 rounded-xl cursor-pointer">
-              Finance
-            </li>
-          </Link>
-          <Link to={"/manager/archieve/leavesArchieve"}>
-            <li class="px-4 py-2 text-gray-300 hover:bg-cyan-400/10 hover:text-cyan-300 rounded-xl cursor-pointer">
-              Leaves
-            </li>
-          </Link>
-          <Link to={"/manager/archieve/vacationsArchieve"}>
-            <li class="px-4 py-2 text-gray-300 hover:bg-cyan-400/10 hover:text-cyan-300 rounded-xl cursor-pointer">
-              Vacations
-            </li>
-          </Link>
-        
         </ul>
       </nav>
       <section class="bg-[#0b1220] text-white w-100% min-h-140 p-5 rounded-xl mt-2">
         <Outlet />
       </section>
     </>
-  );
+  )
 }
 
-export default Archieve;
+export default Archieve
--- src/role/Manager/LeavesArchieve.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function LeavesArchieve() {
-  return <div>LeavesArchieve</div>;
-}
-
-export default LeavesArchieve
